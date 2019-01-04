e9009f26d0cc21f3d805c1e56e4625bf1a9b390a
Mark <14786851+memersonloh@users.noreply.github.com>
2019-01-04 14:25:35 +0800

    Add files via upload

--- src/java/service/MemberWS.java
+++ src/java/service/MemberWS.java
@@ -18,13 +18,13 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.*;
 import java.util.ArrayList;
+import javax.ws.rs.POST;
 import javax.ws.rs.core.GenericEntity;
 
 /**
  * REST Web Service
- *
- * @author lyery
  */
+
 @Path("memberWS")
 public class MemberWS {
 
@@ -41,9 +41,7 @@ public class MemberWS {
      * Retrieves representation of an instance of service.MemberWS
      * @return an instance of java.lang.String
      */
-    
-    
-    
+       
     //Done by Ryan Lye p1638611
     @GET
     @Path("getMember")
@@ -105,17 +103,41 @@ public class MemberWS {
         }
  }
     
+    //Done by Mark Loh p1636846
+    @Path("updateProfile")
+    @POST
+    @Consumes("application/json")
+    //@Produces("application/json")
+    public Response updateProfile(@QueryParam("name") String name, @QueryParam("email") String email, @QueryParam("phone") String phone, @QueryParam("city") String city, @QueryParam("address") String address,
+    @QueryParam("securityQuestion")int securityQuestion, @QueryParam("securityAnswer") String securityAnswer, @QueryParam("age") int age, @QueryParam("income")int income) {
+        try {
+            //Class.forName("com.sql.jdbc.Driver");
+            String connURL = "jdbc:mysql://localhost/islandfurniture-it07?user=root&password=12345";
+            Connection conn = DriverManager.getConnection(connURL);
+            //String stmt = "UPDATE memberentity set name=? where email=?";
+            String stmt = "UPDATE memberentity SET name=?, phone=?, city=?, address=?, securityquestion=?, securityanswer=?, age=?, income=? WHERE email=?";
+            PreparedStatement ps = conn.prepareStatement(stmt);
+            
+            ps.setString(1, name);
+            ps.setString(2, phone);
+            ps.setString(3, city);
+            ps.setString(4, address);
+            ps.setInt(5, securityQuestion);
+            ps.setString(6, securityAnswer);
+            ps.setInt(7,age);
+            ps.setInt(8,income);
+            ps.setString(9,email);
+            int result = ps.executeUpdate();
+            
+            if (result > 0) {
+                return Response.status(Response.Status.OK).build();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
     /**
      * PUT method for updating or creating an instance of MemberWS
      * @param content representation for the resource
@@ -124,6 +146,4 @@ public class MemberWS {
     @Consumes(MediaType.APPLICATION_XML)
     public void putXml(String content) {
     }
-
-    
 }
